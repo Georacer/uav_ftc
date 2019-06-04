bce599b64ea8b1af181cb7381bde69733dfbc2af
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2019-06-04 16:42:16 +0300

    Added axes labels in plot2_points

--- include/uav_ftc/plot_utils.py
+++ include/uav_ftc/plot_utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
-def plot3_points(point_array):
+def plot3_points(point_array, axes_names=['x', 'y', 'z']):
     x = point_array[:,0]
     y = point_array[:,1]
     z = point_array[:,2]
@@ -10,6 +10,10 @@ def plot3_points(point_array):
     fig = plt.figure()
     ax = fig.add_subplot('111', projection='3d')
     ax.scatter(x, y, z, c='b', marker='o')
+
+    ax.set_xlabel(axes_names[0])
+    ax.set_ylabel(axes_names[1])
+    ax.set_zlabel(axes_names[2])
 
     plt.show()
 
