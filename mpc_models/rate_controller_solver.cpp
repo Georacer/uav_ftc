007b4c9507a7f0f613c7e6d69d261fd2846d0e06
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2019-06-06 21:34:49 +0300

    Cleaned up rate_controller_solver Does not run in simulation, due to OnlineData variables

--- mpc_models/rate_controller_solver.cpp
+++ mpc_models/rate_controller_solver.cpp
@@ -18,15 +18,17 @@ int main()
   and solved on execution. The reference and constraints must be set in here.
 */
 
-  const bool CODE_GEN = false;
+  const bool CODE_GEN = true;
 
+  ///////////////////
   // System variables
+  ///////////////////
   DifferentialState p, q, r;
   Control da, de, dr;
-  // OnlineData Va, alpha, beta;
-  const double Va = 15;
-  const double alpha = 0.03;
-  const double beta = 0;
+  OnlineData Va, alpha, beta;
+  // const double Va = 15;
+  // const double alpha = 0.03;
+  // const double beta = 0;
 
   // Parameters which are to be set/overwritten at runtime
   const double t_start = 0.0;      // Initial time [s]
@@ -85,9 +87,10 @@ int main()
   ///////////////
   DifferentialEquation f;
   // Torque calculations
-  IntermediateState l = 0.5 * rho * Va * Va * S * b * (c_l_0 + c_l_b * beta + b / 2 / Va * (c_l_p * p + c_l_r * r) + c_l_deltaa * da + c_l_deltar * dr);
-  IntermediateState m = 0.5 * rho * Va * Va * S * c * (c_m_0 + c_m_a * alpha + c / 2 / Va * (c_m_q * q) + c_m_deltae * de);
-  IntermediateState n = 0.5 * rho * Va * Va * S * b * (c_n_0 + c_n_b * beta + b / 2 / Va * (c_n_p * p + c_n_r * r) + c_n_deltaa * da + c_n_deltar * dr);
+  Expression qbar = 0.5 * rho * Va * Va;
+  IntermediateState l = qbar * S * b * (c_l_0 + c_l_b * beta + b / 2 / Va * (c_l_p * p + c_l_r * r) + c_l_deltaa * da + c_l_deltar * dr);
+  IntermediateState m = qbar * S * c * (c_m_0 + c_m_a * alpha + c / 2 / Va * (c_m_q * q) + c_m_deltae * de);
+  IntermediateState n = qbar * S * b * (c_n_0 + c_n_b * beta + b / 2 / Va * (c_n_p * p + c_n_r * r) + c_n_deltaa * da + c_n_deltar * dr);
 
   // System dynamics
   f << dot(p) == G1 * p * q - G2 * q * r + G3 * l + G4 * n;
@@ -113,12 +116,12 @@ int main()
   // Running cost weight matrix
   DMatrix Q(h.getDim(), h.getDim());
   Q.setIdentity();
-  Q(0, 0) = 1000; // p
-  Q(1, 1) = 1000; // q
-  Q(2, 2) = 1000; // r
-  Q(3, 3) = 1; // da
-  Q(4, 4) = 1; // de
-  Q(5, 5) = 1; // dr
+  Q(0, 0) = 100; // p
+  Q(1, 1) = 100; // q
+  Q(2, 2) = 100; // r
+  Q(3, 3) = 1;   // da
+  Q(4, 4) = 1;   // de
+  Q(5, 5) = 1;   // dr
 
   // End cost weight matrix
   DMatrix QN(hN.getDim(), hN.getDim());
@@ -127,42 +130,10 @@ int main()
   QN(1, 1) = Q(1, 1);
   QN(2, 2) = Q(2, 2);
 
-  /////////////////////////////////////////////////////////
-  // Set a reference for the analysis, if CODE_GEN == false
-  //////////////////////////////////////////////////////////
-  DVector ref(h.getDim());
-  ref.setZero();
-  ref(0) = 0;
-  ref(1) = 0;
-  ref(2) = 0; // Yaw rate required at 0rad/s
-
-  DVector refN(hN.getDim());
-  refN.setZero();
-  refN(0) = ref(0);
-  refN(1) = ref(1);
-  refN(2) = ref(2);
-
   ////////////////////////////////////
   // Define an optimal control problem
   ////////////////////////////////////
   OCP ocp(t_start, t_end, N);
-  if (!CODE_GEN)
-  {
-    // For analysis, set references
-    ocp.minimizeLSQ(Q, h, ref);
-    ocp.minimizeLSQEndTerm(QN, hN, refN);
-  }
-  else
-  {
-    // For code generation, references are set during run time
-    BMatrix Q_sparse(h.getDim(), h.getDim());
-    Q_sparse.setIdentity();
-    BMatrix QN_sparse(hN.getDim(), hN.getDim());
-    QN_sparse.setIdentity();
-    ocp.minimizeLSQ(Q_sparse, h);
-    ocp.minimizeLSQEndTerm(QN_sparse, hN);
-  }
-
   // Add system dynamics
   ocp.subjectTo(f);
   // Add constraints
@@ -170,8 +141,42 @@ int main()
   ocp.subjectTo(-deltae_max <= de <= deltae_max);
   ocp.subjectTo(-deltar_max <= dr <= deltar_max);
   // Set Number of Online Data
-  // ocp.setNOD(3);
+  ocp.setNOD(3);
+  if (!CODE_GEN)
+  {
+    // Set the values of online data
+    // ocp.subjectTo(Va == 15);
+    // ocp.subjectTo(alpha == 0.035);
+    // ocp.subjectTo(beta == 0);
 
+    // Set a reference for the analysis
+    DVector ref(h.getDim());
+    ref.setZero();
+    // Set all angular rates to 0
+    ref(0) = 0;
+    ref(1) = 0;
+    ref(2) = 0;
+
+    DVector refN(hN.getDim());
+    refN.setZero();
+    refN(0) = ref(0);
+    refN(1) = ref(1);
+    refN(2) = ref(2);
+
+    // Set the references into the problem
+    ocp.minimizeLSQ(Q, h, ref);
+    ocp.minimizeLSQEndTerm(QN, hN, refN);
+  }
+  else
+  {
+    // For code generation, references are set during run time
+    ocp.minimizeLSQ(Q, h);
+    ocp.minimizeLSQEndTerm(QN, hN);
+  }
+
+  ////////////////////
+  // System simulation
+  ////////////////////
   if (!CODE_GEN)
   {
     // Setup the simulated process
@@ -202,13 +207,11 @@ int main()
       exit(EXIT_FAILURE);
     }
 
-    // Plot the results
+    // Plot the results at the end of the simulation
     VariablesGrid sampledProcessOutput;
     sim.getSampledProcessOutput(sampledProcessOutput);
-
     VariablesGrid feedbackControl;
     sim.getFeedbackControl(feedbackControl);
-
     GnuplotWindow window;
     window.addSubplot(sampledProcessOutput(0), "p");
     window.addSubplot(sampledProcessOutput(1), "q");
@@ -217,28 +220,6 @@ int main()
     window.addSubplot(feedbackControl(1), "de");
     window.addSubplot(feedbackControl(2), "dr");
     window.plot();
-    // // Set initial state
-    // ocp.subjectTo(AT_START, p == 0);
-    // ocp.subjectTo(AT_START, q == 0);
-    // ocp.subjectTo(AT_START, r == 0);
-
-    // // Setup visualizations
-    // GnuplotWindow window1(PLOT_AT_EACH_ITERATION);
-    // window1.addSubplot(p, "roll rate (rad/s)");
-    // window1.addSubplot(q, "pitch rate (rad/s)");
-    // window1.addSubplot(r, "yaw rate (rad/s)");
-    // GnuplotWindow window2(PLOT_AT_EACH_ITERATION);
-    // window1.addSubplot(da, "aileron input (rad)");
-    // window1.addSubplot(de, "elevator input (rad)");
-    // window1.addSubplot(dr, "rudder input (rad)");
-
-    // // Define an algorithm to solve the problem
-    // OptimizationAlgorithm algorithm(ocp);
-    // algorithm.set(INTEGRATOR_TOLERANCE, 1e-6);
-    // algorithm.set(KKT_TOLERANCE, 1e-3);
-    // algorithm << window1;
-    // algorithm << window2;
-    // algorithm.solve();
   }
   else
   {
@@ -258,8 +239,8 @@ int main()
     mpc.set(USE_SINGLE_PRECISION, YES);             // Single precision
 
     // Do not generate tests, makes or matlab-related interfaces.
-    mpc.set(GENERATE_TEST_FILE, NO);
-    mpc.set(GENERATE_MAKE_FILE, NO);
+    mpc.set(GENERATE_TEST_FILE, YES);
+    mpc.set(GENERATE_MAKE_FILE, YES);
     mpc.set(GENERATE_MATLAB_INTERFACE, NO);
     mpc.set(GENERATE_SIMULINK_INTERFACE, NO);
 
