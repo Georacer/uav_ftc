2d0ae3b20e75bcdb1eedcfa4f2dd31ea57cfcc4b
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2019-06-10 11:37:47 +0300

    Fleshed out rate_controller

--- include/rate_controller.hpp
+++ include/rate_controller.hpp
@@ -1,2 +1,48 @@
 #include <ros/ros.h>
 #include "rate_controller_wrapper.hpp"
+
+#include <cstdlib>
+#include <math.h>
+#include <geometry_msgs/Vector3.h>
+
+#include <mathutils/mathutils.hpp>
+#include <uav_utils/uav_utils.hpp>
+#include <last_letter_msgs/SimStates.h>
+
+class RateController
+{
+public:
+    ///////////
+    //Variables
+    last_letter_msgs::SimStates states;
+    geometry_msgs::Vector3 airdata;
+    uav_ftc::refRates refRates;
+    double input[10]; // Stores control inputs, -1,1 range
+    double output[10];  // Stores control outputs, -1,1 range
+    ros::Time tprev;
+    ros::Subscriber subState, subRef;
+    ros::Publisher pubCtrl;
+
+    /////////
+    //Members
+    RateMpcWrapper mpcController();
+
+    ///////////
+    //Functions
+    void step();                                                 // Caller of rate_controller_wrapper
+    void getStates(last_letter_msgs::SimStates measuredStates); // Callback to store measured states
+    void getReference(uav_ftc::ref_rates reference);             // Callback to store reference command  //TODO: Declare and compile the msg
+    void WriteOutput();                            // Send control signals to the control inputs aggregator
+
+    //Constructor
+    RateController(ros::NodeHandle n);
+    //Destructor
+    ~RateController();
+
+private:
+    ///////////
+    //Variables
+
+    ///////////
+    //Functions
+};
--- src/rate_controller.cpp
+++ src/rate_controller.cpp
@@ -1,5 +1,80 @@
 #include "rate_controller.hpp"
 
+///////////////////
+//Class Constructor
+RateController::RateController(ros::NodeHandle n)
+{
+    //Initialize states
+    tprev = ros::Time::now();
+    states.header.stamp = tprev;
+
+    //Subscribe and advertize
+    subState = n.subscribe("states", 1, &RateController::getStates, this);
+    subRef = n.subscribe("refRates", 1, &RateController::getReference, this);
+    pubCtrl = n.advertise<geometry_msgs::Vector3>("ctrlSurfaceCmds", 100);
+
+    // RateMpcController already initialized in declaration
+}
+
+///////////////////
+//Class Destructor
+RateController::~RateController()
+{
+    delete mpcController;
+}
+
+////////////
+// Main Step
+void RateController::step()
+{
+    // Copy over p, q, r
+    geometry_msgs::Vector3 currentState = states.velocity.angular;
+
+    // Convert airdata triplet
+    airdata = getAirData(states.velocity.linear);
+
+    // refRates are already saved
+
+    // Set mpc OnlineData with airdata
+    // Set mpc x0 with states
+    // Set mpc reference with refRates
+
+    float output[4];
+    // Fill control commands and publish them
+    writePWM(output);
+}
+
+///////////
+//Utilities
+///////////
+
+void RateController::getStates(last_letter_msgs::SimStates inpStates)
+{
+    states = inpStates;
+}
+
+///////////////////////////////////////////////////////
+// Controller outputs to us PPM values and publish them
+void RateController::writeOutput()
+{
+    last_letter_msgs::SimPWM channels;
+    channels.value[0] = FullRangeToPwm(output[0]);
+    channels.value[1] = FullRangeToPwm(output[1]);
+    channels.value[2] = HalfRangeToPwm(output[2]);
+    channels.value[3] = FullRangeToPwm(output[3]);
+    channels.value[9] = HalfRangeToPwm(output[9]);
+    channels.header.stamp = ros::Time::now();
+    pubCtrl.publish(channels);
+}
+
+/////////////////////////////////////////////////
+// Store reference commands
+void RateController::getReference(uav_ftc::refRates pRefRates)
+{
+    refRates = pRefRates;
+}
+
+RateMpcWrapper<float> rate_ctrl();
 
 ///////////////
 //Main function
@@ -12,21 +87,19 @@ int main(int argc, char **argv)
 
     ros::WallDuration(3).sleep(); //wait for other nodes to get raised
     double ctrlRate;
-    ros::param::get("ctrlRate",ctrlRate); //frame rate in Hz
+    ros::param::get("ctrlRate", ctrlRate); //frame rate in Hz
     ros::Rate spinner(ctrlRate);
 
-    RateMpcWrapper<float> rate_ctrl();
+    RateController rate_ctrl(n);
     spinner.sleep();
     ROS_INFO("controlNode up");
 
     while (ros::ok())
     {
         ros::spinOnce();
-        // rate_ctrl.step();
+        rate_ctrl.step();
         spinner.sleep();
-
     }
 
     return 0;
-
 }
