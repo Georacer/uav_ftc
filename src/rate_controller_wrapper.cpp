daf93a09623517772cb4de7744162a2bf97adee6
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2019-06-10 21:29:49 +0300

    Removed debug output and commented-out code

--- src/rate_controller_wrapper.cpp
+++ src/rate_controller_wrapper.cpp
@@ -34,21 +34,11 @@ RateMpcWrapper<T>::RateMpcWrapper()
     acado_reference_end_state_.segment(0, kStateSize) =
         trim_state.template cast<float>();
 
-    //   // Initialize Cost matrix W and WN.
-    //   if(!(acado_W_.trace()>0.0))
-    //   {
-    //     acado_W_ = W_.replicate(1, kSamples).template cast<float>();
-    //     acado_W_end_ = WN_.template cast<float>();
-    //   }
-
     // Initialize online data.
     float Va = 15;
     float alpha = 0.034;
     float beta = 0.0;
     Eigen::Matrix<T, kOdSize, 1> online_data(Va, alpha, beta);
-    //   Eigen::Matrix<T, 3, 1> p_B_C(0, 0, 0);
-    //   Eigen::Quaternion<T> q_B_C(1, 0, 0, 0);
-    //   Eigen::Matrix<T, 3, 1> point_of_interest(0, 0, -1000);
 
     setOnlineData(online_data);
 
@@ -57,97 +47,6 @@ RateMpcWrapper<T>::RateMpcWrapper()
     acado_preparationStep();
     acado_is_prepared_ = true;
 }
-
-// // Constructor with cost matrices as arguments.
-// template <typename T>
-// RateMpcWrapper<T>::RateMpcWrapper(
-//   const Eigen::Ref<const Eigen::Matrix<T, kCostSize, kCostSize>> Q,
-//   const Eigen::Ref<const Eigen::Matrix<T, kInputSize, kInputSize>> R)
-// {
-//   setCosts(Q, R);
-//   RateMpcWrapper();
-// }
-
-// // Set cost matrices with optional scaling.
-// template <typename T>
-// bool RateMpcWrapper<T>::setCosts(
-//   const Eigen::Ref<const Eigen::Matrix<T, kCostSize, kCostSize>> Q,
-//   const Eigen::Ref<const Eigen::Matrix<T, kInputSize, kInputSize>> R,
-//   const T state_cost_scaling, const T input_cost_scaling)
-// {
-//   if(state_cost_scaling < 0.0 || input_cost_scaling < 0.0 )
-//   {
-//     ROS_ERROR("MPC: Cost scaling is wrong, must be non-negative!");
-//     return false;
-//   }
-//   W_.block(0, 0, kCostSize, kCostSize) = Q;
-//   W_.block(kCostSize, kCostSize, kInputSize, kInputSize) = R;
-//   WN_ = W_.block(0, 0, kCostSize, kCostSize);
-
-//   float state_scale{1.0};
-//   float input_scale{1.0};
-//   for(int i=0; i<kSamples; i++)
-//   {
-//     state_scale = exp(- float(i)/float(kSamples)
-//       * float(state_cost_scaling));
-//     input_scale = exp(- float(i)/float(kSamples)
-//       * float(input_cost_scaling));
-//     acado_W_.block(0, i*kRefSize, kCostSize, kCostSize) =
-//       W_.block(0, 0, kCostSize, kCostSize).template cast<float>()
-//       * state_scale;
-//     acado_W_.block(kCostSize, i*kRefSize+kCostSize, kInputSize, kInputSize) =
-//       W_.block(kCostSize, kCostSize, kInputSize, kInputSize
-//         ).template cast<float>() * input_scale;
-//   }
-//   acado_W_end_ = WN_.template cast<float>() * state_scale;
-
-//   return true;
-// }
-
-// // Set the input limits.
-// template <typename T>
-// bool RateMpcWrapper<T>::setLimits(T min_thrust, T max_thrust,
-//     T max_rollpitchrate, T max_yawrate)
-// {
-//   if(min_thrust <= 0.0 || min_thrust > max_thrust)
-//   {
-//     ROS_ERROR("MPC: Minimal thrust is not set properly, not changed.");
-//     return false;
-//   }
-
-//   if(max_thrust <= 0.0 || min_thrust > max_thrust)
-//   {
-//     ROS_ERROR("MPC: Maximal thrust is not set properly, not changed.");
-//     return false;
-//   }
-
-//   if(max_rollpitchrate <= 0.0)
-//   {
-//     ROS_ERROR("MPC: Maximal xy-rate is not set properly, not changed.");
-//     return false;
-//   }
-
-//   if(max_yawrate <= 0.0)
-//   {
-//     ROS_ERROR("MPC: Maximal yaw-rate is not set properly, not changed.");
-//     return false;
-//   }
-
-//   // Set input boundaries.
-//   Eigen::Matrix<T, 4, 1> lower_bounds = Eigen::Matrix<T, 4, 1>::Zero();
-//   Eigen::Matrix<T, 4, 1> upper_bounds = Eigen::Matrix<T, 4, 1>::Zero();
-//   lower_bounds << min_thrust,
-//     -max_rollpitchrate, -max_rollpitchrate, -max_yawrate;
-//   upper_bounds << max_thrust,
-//     max_rollpitchrate, max_rollpitchrate, max_yawrate;
-
-//   acado_lower_bounds_ =
-//     lower_bounds.replicate(1, kSamples).template cast<float>();
-
-//   acado_upper_bounds_ =
-//     upper_bounds.replicate(1, kSamples).template cast<float>();
-//   return true;
-// }
 
 // Set online data
 template <typename T>
@@ -239,16 +138,9 @@ bool RateMpcWrapper<T>::update(
     // Pass measurement
     setInitialState(state);
 
-    std::cout << "Ready to perform solver step" << std::endl;
-    std::cout << "Initial State: " << acado_initial_state_ << std::endl;
-    std::cout << "Online data: " << acado_online_data_ << std::endl;
-    std::cout << "Reference: " << acado_reference_states_ << std::endl;
-
     // Perform feedback step and reset preparation check.
     acado_feedbackStep();
     acado_is_prepared_ = false;
-
-    std::cout << "Resulting input: " << acado_inputs_ << std::endl;
 
     // Prepare if the solver if wanted
     if (do_preparation)
