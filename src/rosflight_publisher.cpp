23723c38ab71d336ad18a80678463e6f1349be35
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2020-04-12 13:53:42 +0300

    Added compilation target for rosflight_publisher
    Also corrected typos and bugs

--- src/rosflight_publisher.cpp
+++ src/rosflight_publisher.cpp
@@ -8,11 +8,12 @@
  * 
  */
 
-#include <math_utils.hpp>
+#include <ros/ros.h>
 
 #include <geometry_msgs/Vector3Stamped.h>
 #include <rosflight_msgs/Command.h>
 
+#include <math_utils.hpp>
 #include <last_letter_msgs/SimPWM.h>
 
 ros::Publisher pub;
@@ -20,17 +21,17 @@ ros::Publisher pub;
 /**
  * @brief Callback to the control surfaces message
  * 
- * @param data_bus The control surfaces message
+ * @param pwm_msg The control surfaces message
  */
-void rcCallback(last_letter_msgs::SimPWM)
+void rcCallback(last_letter_msgs::SimPWM pwm_msg)
 {
     rosflight_msgs::Command new_msg;
-    new_msg.mode = msg.MODE_PASS_THROUGH;
-    new_msg.ignore = msg.IGNORE_NONE;
-    new_msg.x = data_bus.rc_in[0];
-    new_msg.y = data_bus.rc_in[1];
-    new_msg.F = data_bus.rc_in[2];
-    new_msg.z = data_bus.rc_in[3];
+    new_msg.mode = new_msg.MODE_PASS_THROUGH;
+    new_msg.ignore = new_msg.IGNORE_NONE;
+    new_msg.x = pwm_msg.value[0];
+    new_msg.y = pwm_msg.value[1];
+    new_msg.F = pwm_msg.value[2];
+    new_msg.z = pwm_msg.value[3];
 
     pub.publish(new_msg);
 }
@@ -45,8 +46,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "rosflight_publisher");
     ros::NodeHandle n;
 
-    ros::Subscriber sub = n.subscribe("ctrlPWM", 1, rcCallback, this);
-    pub = n.advertise<rosfliht_msgs::>("/command", 1);
+    ros::Subscriber sub = n.subscribe("ctrlPWM", 1, rcCallback);
+    pub = n.advertise<rosflight_msgs::Command>("/command", 1);
 
     ROS_INFO("rosflight_publisher node up");
 
