c251841d509d9621984ea47e400cb972c23e0bf6
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2020-04-14 12:41:08 +0300

    rosflight_publisher: Added scaling arguments

--- src/rosflight_publisher.cpp
+++ src/rosflight_publisher.cpp
@@ -18,6 +18,10 @@
 #include <last_letter_msgs/SimPWM.h>
 
 ros::Publisher pub;
+double x_scale{1.0};
+double y_scale{1.0};
+double z_scale{1.0};
+double f_scale{1.0};
 
 /**
  * @brief Callback to the control surfaces message
@@ -29,10 +33,10 @@ void rcCallback(last_letter_msgs::SimPWM pwm_msg)
     rosflight_msgs::Command new_msg;
     new_msg.mode = new_msg.MODE_PASS_THROUGH;
     new_msg.ignore = new_msg.IGNORE_NONE;
-    new_msg.x = PwmToFullRange(pwm_msg.value[0]);
-    new_msg.y = PwmToFullRange(pwm_msg.value[1]);
-    new_msg.F = PwmToHalfRange(pwm_msg.value[2]);
-    new_msg.z = -PwmToFullRange(pwm_msg.value[3]); // Invert sign because of topology of DeFault UAV
+    new_msg.x =  x_scale*PwmToFullRange(pwm_msg.value[0]);
+    new_msg.y =  y_scale*PwmToFullRange(pwm_msg.value[1]);
+    new_msg.F =  f_scale*PwmToHalfRange(pwm_msg.value[2]);
+    new_msg.z = -z_scale*PwmToFullRange(pwm_msg.value[3]); // Invert sign because of topology of DeFault UAV
 
     pub.publish(new_msg);
 }
@@ -50,7 +54,15 @@ int main(int argc, char **argv)
     ros::Subscriber sub = n.subscribe("ctrlPWM", 1, rcCallback);
     pub = n.advertise<rosflight_msgs::Command>("command", 1);
 
+    ros::NodeHandle pnh("~");
+
+    pnh.param("xscale", x_scale, 1.0);
+    pnh.param("yscale", y_scale, 1.0);
+    pnh.param("zscale", z_scale, 1.0);
+    pnh.param("fscale", f_scale, 1.0);
+
     ROS_INFO("rosflight_publisher node up");
+    ROS_INFO("With scales %f, %f, %f, %f", x_scale, y_scale, z_scale, f_scale);
 
     while (ros::ok())
     {
